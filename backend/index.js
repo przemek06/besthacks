ca399b651d88bf42eca148f01196b5d0d1b103fe
Dawid <dawid.ziobrowski@wp.pl>
2020-10-24 18:46:24 +0200

    Logowanie auth0

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,34 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { auth } from 'express-openid-connect';
+import { requiresAuth } from 'express-openid-connect';
+import axios from 'axios';
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//TODO skorygować config
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: 'http://localhost:3000',
+    clientID: 'w70E9o8uRt6HRVO3vPf84ScBuX92J5Rb',
+    issuerBaseURL: 'https://dev-uxai7-vh.eu.auth0.com',
+    secret: 'LONG_RANDOM_STRING'
+};
 
-//testowy get
+app.use(auth(config));
+
 app.get('/', (req, res) => {
-    res.json({status: "success", message: "aplikacja działa jak należy", data: null});
+    res.json(req.oidc.isAuthenticated() ? {status: "success", message: "Logged in", data: null} : {status: "success", message:"Logged out", data: null});
 });
 
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.json({status: "success", message: "profil", data:req.oidc.user});
+});
 
 //express nie ogarnia błędu 404
 app.use((req, res, next) => {
@@ -30,8 +46,6 @@ app.use((err, req, res, next) => {
         res.status(500).json({message: "Coś poszło nie tak! :("});
 
 });
-
-
 
 
 app.listen(port, () => {
